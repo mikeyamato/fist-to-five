e035fc42c0bb102df0697460d1a38770878340c9
Mix Master Mike <mikeyamato@gmail.com>
2018-07-19 19:28:38 -0700

    added post response

--- index.js
+++ index.js
@@ -12,3 +12,16 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
 	console.log('Listening on port...', port);
 });
+
+app.post('/hello', (req, res, next) => {
+	const userName = req.body.user_name;
+	const botPayload = {
+		text: 'Hello' + userName + ', you\'re looking good!'
+	}
+
+	if (userName !== 'slackbot') {
+		return res.status(200).json(botPayload)
+	} else {
+		return res.status(200).end()
+	}
+});
